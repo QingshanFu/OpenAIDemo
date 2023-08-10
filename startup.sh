cd1bceb5e827c8541c564be6bf55c2711e71619f
傅庆山 <42727537+QingshanFu@users.noreply.github.com>
2023-08-10 16:52:42 +0800

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Streamlit if not already installed
+if ! command -v streamlit &> /dev/null
+then
+    echo "Streamlit not found, installing..."
+    pip install streamlit
+fi
+
+# Run the Streamlit app
+streamlit run App.py  --server.port 8000 --server.address 0.0.0.0
